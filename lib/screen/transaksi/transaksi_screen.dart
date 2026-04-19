e06b2d0158e6f7e853f091f1e890ec500dfb21b4
Endraagustino22 <endraagustino2018@gmail.com>
2026-04-19 20:22:22 +0700

    feat: add LaporanScreenWidget with SaldoSummaryWidget

---
+++ lib/screen/transaksi/transaksi_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pencatatan_keuangan_app/screen/dashboard/widgets/jumlah_saldo.dart';
+
+class LaporanScreenWidget extends StatefulWidget {
+  const LaporanScreenWidget({super.key});
+
+  @override
+  State<LaporanScreenWidget> createState() => _LaporanScreenWidgetState();
+}
+
+class _LaporanScreenWidgetState extends State<LaporanScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SaldoSummaryWidget(),
+      ],
+    );
+  }
+}
