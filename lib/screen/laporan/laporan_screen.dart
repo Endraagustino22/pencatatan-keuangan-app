2b59f6cbdedae5f87724c67119c559ed1dd4dd54
Endraagustino22 <endraagustino2018@gmail.com>
2026-04-19 20:13:55 +0700

    refactor: remove unused imports di laporanscreen.dart

--- lib/screen/laporan/laporan_screen.dart
+++ lib/screen/laporan/laporan_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pencatatan_keuangan_app/model/transaksi.dart';
 import 'package:pencatatan_keuangan_app/screen/dashboard/widgets/jumlah_saldo.dart';
 import 'package:pencatatan_keuangan_app/screen/laporan/widgets/tab_view.dart';
-import 'package:pencatatan_keuangan_app/screen/laporan/widgets/transaksi_list.dart';
 
 class LaporanScreenWidget extends StatefulWidget {
   const LaporanScreenWidget({super.key});
